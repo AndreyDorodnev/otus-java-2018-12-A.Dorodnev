5966ad702299b8bb77c402c53e7042deade69f1e
Andrey Dorodnev <andrew992@mail.ru>
2019-03-24 12:50:32 +0300

    classes type fix

--- L101myORM/src/main/java/base/DBService.java
+++ L101myORM/src/main/java/base/DBService.java
@@ -1,11 +1,13 @@
 package base;
 
+import model.DataSet;
+
 import java.sql.SQLException;
 
 public interface DBService {
     String getMetaData() throws SQLException;
 
-    void prepareTables(Class[] classes) throws SQLException;
+    void prepareTables(Class<? extends DataSet>[] classes) throws SQLException;
 
-    void deleteTables(Class[] classes) throws SQLException;
+    void deleteTables(Class<? extends DataSet>[] classes) throws SQLException;
 }
